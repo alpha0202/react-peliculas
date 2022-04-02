cf952ee6807ea5dc2d384db5ad0d19b39776dd96
alpha0202 <alpha0202@gmail.com>
2022-04-02 13:58:46 -0500

    Primero commit de proyecto peliculas con react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const subTitulo = "este es un subtitulo";
+
+  const duplicar = (valor: number)=>valor*2;
+
+  const imgUrl = "https://res.cloudinary.com/practicaldev/image/fetch/s--qo_Wp38Z--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/e0nl7ziy1la7bpwj7rsp.png"
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="rojo">Hola a todos</h1>
+      <div className="cuadradoRojo"></div>
+
+      <h3>{subTitulo.toUpperCase()}</h3>
+      <h4>el doble de 3 es {duplicar(3)}</h4>
+      <img alt="logo react" src={imgUrl}></img>
+      <div>
+        <input type="checkbox" checked={false} /> este es un checkbox.
+      </div>
+    </>
   );
 }
 
